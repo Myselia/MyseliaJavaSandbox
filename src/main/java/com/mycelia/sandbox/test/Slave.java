5a23695eb386961c7e0bcafa5270952086b4436c
Eduard Paraschivescu <ed.orangeduke@gmail.com>
2015-04-11 16:37:59 -0400

    Added slave code

--- src/main/java/com/mycelia/sandbox/test/Slave.java
+++ src/main/java/com/mycelia/sandbox/test/Slave.java
@@ -1,8 +1,18 @@
 package com.mycelia.sandbox.test;
 
+import java.util.Random;
+
+import com.mycelia.common.communication.units.TransmissionBuilder;
+import com.mycelia.common.constants.opcode.ActionType;
+import com.mycelia.common.constants.opcode.ComponentType;
+import com.mycelia.common.constants.opcode.OpcodeAccessor;
 import com.mycelia.sandbox.runtime.templates.MyceliaSlaveModule;
 
 public class Slave extends MyceliaSlaveModule{
+	TransmissionBuilder tb = new TransmissionBuilder();
+	
+	Random random = new Random();
+	private int rand = 0;
 	
 	public Slave(){
 		System.out.println("SLAVE CREATED");
@@ -10,13 +20,29 @@ public class Slave extends MyceliaSlaveModule{
 
 	@Override
 	public void setup() {
-		// TODO Auto-generated method stub
+
 	}
 
 	@Override
 	protected void tick() {
-		// TODO Auto-generated method stub
+		new_number();
 		
+		tb.newTransmission(
+				OpcodeAccessor.make(ComponentType.SANDBOXSLAVE, ActionType.RUNTIME, "DATA"), 
+				OpcodeAccessor.make(ComponentType.LENS, ActionType.RUNTIME, "DATA"));
+		tb.addAtom("random", "int", Integer.toString(rand));
+		mailbox.putInOutQueue(tb.getTransmission());
+		
+		try {
+			Thread.sleep(200);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	private void new_number(){
+		this.rand = random.nextInt(9);
 	}
 
 }
