35360e574e404d2754cebeac49d9393285586f73
Vynny <sylvain.czyzewski@gmail.com>
2015-09-12 14:45:04 -0400

    generic git commit comment

--- src/main/java/com/myselia/sandbox/runtime/network/NetworkRuntime.java
+++ src/main/java/com/myselia/sandbox/runtime/network/NetworkRuntime.java
@@ -46,8 +46,6 @@ public class NetworkRuntime extends MyseliaRuntime {
 			componentcommunicator = new ComponentCommunicator(componenttranslation(moduleType));
 			componentcommunicator.start();
 			
-
-			
 			try{
 				if(moduleType.equals(MyseliaModuleType.MASTER)){
 					module = masterModuleClass.newInstance();
