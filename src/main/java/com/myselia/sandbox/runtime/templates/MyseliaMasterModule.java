420fb0df586d567ee1cf25f421baaa0585de24a9
TheOrangeDuke <ed.orangeduke@gmail.com>
2015-09-17 20:18:43 -0400

    Created Virtual Slave, added aggregated args

--- src/main/java/com/myselia/sandbox/runtime/templates/MyseliaMasterModule.java
+++ src/main/java/com/myselia/sandbox/runtime/templates/MyseliaMasterModule.java
@@ -1,11 +1,29 @@
 package com.myselia.sandbox.runtime.templates;
 
 import com.myselia.sandbox.constants.MyseliaModuleType;
+import com.myselia.sandbox.runtime.network.VirtualSlave;
 
 public abstract class MyseliaMasterModule extends MyseliaModule{
 	
+	public VirtualSlave[] slaves;
+	
 	public MyseliaMasterModule(){
 		super(MyseliaModuleType.MASTER);
+	}
+	
+	public VirtualSlave[] allocateVirtualSlaves(int count, char[] uid){
+		if(uid.length != count){
+			System.err.println("Wrong uid count for VirtualSlave[] allocation");
+			return null;
+		}
+		
+		VirtualSlave[] slaves = new VirtualSlave[count];
+		for(int i = 0; i < count; i++){
+			slaves[i] = new VirtualSlave(uid[i]);
+		}
+		
+		return slaves;
+		
 	}
 
 }
